195a4ecdcb4940b43ccf93a17339ca09282b58f5
Razvan <razvan@ubuntu.(none)>
2013-10-14 11:30:35 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Razvan::Application.config.secret_key_base = 'e58b170916f71d7b7c52a7ac479859eeec8351763ddb6f127366d9fdb019f0a09c7eac338c5703415eb84752007004dbe7d8f9c2420566f288cf3a7c6d226bf4'
